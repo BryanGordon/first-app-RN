105d78fbfe06f4b5b236d912ee09b41603354bcf
BryanGordon <bryanbge92@gmail.com>
2024-04-26 23:39:47 -0500

    Add scroll on Cart file

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, Platform } from "react-native";
+import { View, Text, Platform, FlatList } from "react-native";
+import { UseCart } from "../providers/cartProviders";
 
 const CartScreen = () => {
+  const { items } = UseCart()
+
   return (
     <View>
-      <Text>Cart</Text>
+      <FlatList data={items} />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   )
